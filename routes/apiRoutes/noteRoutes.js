80272877d184764e6db0ab0b91ea6703f2142ff8
afedoriouk <98120553+afedoriouk@users.noreply.github.com>
2022-07-21 11:59:06 -0400

    readme

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,7 @@
 //routing end point response
+
 const router = require("express").Router();
+
 const { noteCreateNew, noteDeleteNote } = require("../../lib/noteFunctions");
 const { notes } = require("../../db/db.json");
 
@@ -8,12 +10,13 @@ router.get("/notes", (req, res) => {
   let saved = notes;
   res.json(saved);
 });
+//post notes
 router.post("/notes", (req, res) => {
   req.body.id = notes.lenght.toString();
   let note = noteCreateNew(req.body, notes);
   res.json(note);
 });
-
+//delete notes
 router.delete("/notes/:id", (req, res) => {
   noteDeleteNote(notes, req.params.id);
   res.json(notes);
